1aec268b7b99533ac5ab1e3c960dc3be70d0e3c0
zhaomingtoo <854424789@qq.com>
2015-07-31 18:50:56 +0800

    This is second commit

--- DouYuSpider-01.py
+++ DouYuSpider-01.py
@@ -1,14 +1,44 @@
-__author__ = 'Troll'
+# -*- coding:utf-8 -*-
+
+"""
+简单的python爬虫工具，用来爬取当前斗鱼LOL板块正在直播的前30名主播名称
+
+Author= 'Troll'
+Version:0.01
+Date: 2015-7-31
+Language:python 2.7.9
+Editor: Pycharm
+Operator: 请阅读README.md
+"""
+
 import urllib2
 import re
 
 class Spider():
+    """类的简要说明
+
+    本类主要用于抓取斗鱼LOL板块前30名主播的名称
+
+    Attributes:
+        url: 用于表示当前争取抓取页面的url
+        anchors: 存储处理好的抓取到的主播名称
+        rank_number: 用于显示排序
+    """
     def __init__(self):
         self.url = "http://www.douyutv.com/directory/game/LOL"
         self.anchors = []
         self.rank_number = 1
 
     def get_page(self):
+        """
+        根据当前页码爬取网页HTML
+
+        Returns:
+            返回抓取到整个页面的HTML(unicode编码)
+
+        Raises:
+            URLError:url引发的异常
+        """
         try:
             my_page = urllib2.urlopen(self.url).read().decode("utf-8")
         except urllib2.URLError, e:
@@ -21,6 +51,13 @@ class Spider():
         return my_page
 
     def find_title(self, my_page):
+        """
+
+        通过返回的整个网页HTML, 正则匹配前100的电影名称
+
+        Args:
+            my_page: 传入页面的HTML文本用于正则匹配
+        """
         temp_anchors = []
         anchors = re.findall(r'<span.*?class="nnt">(.*?)</span>', my_page, re.S)
         for anchor in anchors:
@@ -29,6 +66,11 @@ class Spider():
         self.anchors.extend(temp_anchors)
 
     def start_spider(self):
+        """
+
+        爬虫入口
+
+        """
         my_page = self.get_page()
         self.find_title(my_page)
 
